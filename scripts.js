293931a350a18385812f63f5a0b76f005b40660e
Bruno Rocha de Lima <91151280+brunorochadelima@users.noreply.github.com>
2022-02-03 14:35:37 -0300

    Atualizando funções Javacript

--- scripts.js
+++ scripts.js
@@ -1,42 +1,26 @@
-function opçãoErrada() {
-  document.getElementById("respostaErrada1").style.backgroundColor = "red";
-  document.getElementById("respostaErrada1").style.color = "white";
-  document.getElementById("respostaErrada2").style.backgroundColor = "red";
-  document.getElementById("respostaErrada2").style.color = "white";
-  document.getElementById("respostaErrada3").style.backgroundColor = "red";
-  document.getElementById("respostaErrada3").style.color = "white";
-  alert("Sua resposta está errada")
-}
-
-function opçãoCerta() {
-  document.getElementById("respostaCerta").style.backgroundColor = "green";
-  document.getElementById("respostaCerta").style.color = "white";
-}
-
 function verificar() {
-  var resposta = document.getElementById("respostaCerta");
+   var resposta = document.getElementById("respostaCerta");
   if (resposta.checked) {
     acertos++
     alert("Parabéns você Acertou!");
   } else {
     erros++;
-    alert("Você Errou dessa vez")
+    alert("Você errou, a resposta correta é: " + document.getElementById("respostaCerta").value)
   }
-  window.location.href = "./index2.html";
+  proximaPagina()
+}
+
+function proximaPagina() {
+  if (acertos + erros == 1) {
+    window.location.href = "./index2.html";
+  }
+  if (acertos + erros >= 2) {
+    window.location.href = "./index3.html";
+  }
 }
 
 var erros = 0;
 var acertos = 0;
-
-
-
-
-
-
-
-
-
-
 
 
 
